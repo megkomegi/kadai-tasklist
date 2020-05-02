a3f8a8e28e03af3c12c76155a00a6c52d4e0e6ef
megumi.hirao <megkomegi@gmail.com>
2020-05-02 08:11:22 +0000

    post

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
-    validates :content, presence: true
-    validates :status, presence: true, length: { maximum: 10 }
+    belongs_to :user
+    validates :content, presence: true, length: { maximum: 255 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_21_135139) do
+ActiveRecord::Schema.define(version: 2020_05_02_075212) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
