fe62bf1831374a8f6211ee38d89d35b989977529
megumi.hirao <megkomegi@gmail.com>
2020-05-04 08:40:00 +0000

    revision for tasklist login error revise

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   get 'signup', to: 'users#new'
-  resources :users, only: [:new, :create]
-  resources :tasks, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :users, only: [:new, :create, :show]
+  resources :tasks
 end
